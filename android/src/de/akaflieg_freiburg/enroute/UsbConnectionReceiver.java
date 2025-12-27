a233396a026feb9024a85970f6ae4b382220b03d
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-12-27 19:04:31 +0100

    Update UsbConnectionReceiver.java

--- android/src/de/akaflieg_freiburg/enroute/UsbConnectionReceiver.java
+++ android/src/de/akaflieg_freiburg/enroute/UsbConnectionReceiver.java
@@ -6,25 +6,32 @@ import android.content.Intent;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 
-public class UsbConnectionReceiver extends BroadcastReceiver {
+public class UsbConnectionReceiver extends BroadcastReceiver
+{
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(Context context, Intent intent)
+    {
         String action = intent.getAction();
 
-        if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+        if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action))
+        {
             UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-            if (device != null) {
+            if (device != null)
+            {
                 // Call native C++ function
                 notifyUsbDeviceAttached(
-                    device.getDeviceName(),
-                    device.getVendorId(),
-                    device.getProductId(),
-                    device.getDeviceClass()
+                device.getDeviceName(),
+                device.getVendorId(),
+                device.getProductId(),
+                device.getDeviceClass()
                 );
             }
-        } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+        }
+        else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action))
+        {
             UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-            if (device != null) {
+            if (device != null)
+            {
                 notifyUsbDeviceDetached(device.getDeviceName());
             }
         }
